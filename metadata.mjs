94306240a4835f733a3586665ec21d2b5d8c7016
Diego Inácio <diegodci@gmail.com>
2021-09-16 10:13:11 -0300

    Upload metadata

---
+++ metadata.mjs
@@ -0,0 +1,51 @@
+export const HEADER = {
+  title: `Data Science Notebooks`,
+  description: `A collection of <span class="data-science">Data Science</span> fundamentals and useful <span class="notebooks">python notebooks</span>.`,
+};
+
+export const INDEX = [
+  // ! Data Visualization
+  {
+    id: "Data-Visualization",
+    name: "Data Visualization",
+    description:
+      "Best reporting practices, with a focus on <em>data visualization</em> and <em>storytelling</em>.",
+    notebooks: [
+      {
+        id: "matplotlib_3D",
+        name: "Matplotlib 3D",
+        description: "Examples of 3D visualization using <em>Matplotlib</em>.",
+      },
+      {
+        id: "matplotlib_animation",
+        name: "Matplotlib Animation",
+        description:
+          "Examples of animated visualization using <em>Matplotlib</em>.",
+      },
+      {
+        id: "matplotlib_charts",
+        name: "Matplotlib Charts",
+        description:
+          "Examples of chart visualization using <em>Matplotlib</em>.",
+      },
+      {
+        id: "matplotlib_diagrams",
+        name: "Matplotlib Diagrams",
+        description:
+          "Examples of diagram visualization using <em>Matplotlib</em>.",
+      },
+      {
+        id: "matplotlib_figures",
+        name: "Matplotlib Figures",
+        description:
+          "Examples of figure visualization using <em>Matplotlib</em>.",
+      },
+      {
+        id: "plotly_charts",
+        name: "Plotly Charts",
+        description:
+          "Examples of <em>interactive</em> chart visualization using <em>Plotly</em>.",
+      },
+    ],
+  },
+];
