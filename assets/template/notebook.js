3e6edc3db35a817859f8a8e769b7ef34094bb3a7
Diego Inácio <diegodci@gmail.com>
2022-04-08 20:07:25 -0300

    Update assets template

--- assets/template/notebook.js
+++ assets/template/notebook.js
@@ -25,6 +25,18 @@ if (notebook_link) {
   div_buttons.appendChild(a_button);
 }
 
+// * Report an Issue
+a_button = document.createElement("a");
+a_button.target = "_blank";
+a_button.href =
+  "https://github.com/diegoinacio/data-science-notebooks/issues/new";
+a_button.innerHTML = `
+  <i class="fas fa-comment-dots"></i>
+  <span class="tooltip">
+    Report an <b>Issue</b>
+  </span>`;
+div_buttons.appendChild(a_button);
+
 // * Return to Data Science Notebooks
 a_button = document.createElement("a");
 a_button.href = "https://diegoinacio.github.io/data-science-notebooks-page/";
